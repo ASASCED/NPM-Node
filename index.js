3da8e78f4cc3f53b7f1c706c79077eaab91fda0d
Anix <jesus_heaven117@outlook.com>
2019-05-18 20:13:51 -0500

    Modulo simple de node con NPM

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log('Hola mundo');        
+    },
+    holaPersonalizado( msg ) {
+        console.log(`Hola ${ msg }`);        
+    },
+    holaAlCursoNPM: function() {
+        console.log('Hola al curso NPM');        
+    }
+};
